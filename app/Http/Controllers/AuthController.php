724271a36c4bda1cda08d4e46bf3a46ae3ef81e6
ribeirosacarlos <ribeirosacarlos@gmail.com>
2025-05-22 16:34:11 -0300

    feat: auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user) {
+            return response(['message' => 'User não identificado'], 401);
+        }
+
+        if (!Hash::check($fields['password'], $user->password)) {
+            return response(['message' => 'Credenciais Invalidas'], 401);
+        }
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        
+        return response(['message' => 'Logout realizado com sucesso'], status: 200);
+    }
+}
