5d68711523ed16dba5d68a53a92a5f2f92ee008d
ribeirosacarlos <ribeirosacarlos@gmail.com>
2025-05-22 16:34:02 -0300

    feat: ajustando models

---
+++ app/Models/Transaction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'type',
+        'amount',
+        'related_user_id',
+        'reversed'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function relatedUser()
+    {
+        return $this->belongsTo(User::class, 'related_user_id');
+    }
+}
